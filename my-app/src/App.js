b2f1f5d68cea5a6d5c882ad0b18e4c239ffce699
Raj <raj8827778809@gmail.com>
2025-07-14 18:02:35 +0530

    second-commit

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='bg-blue-500 text-white p-4 rounded'>Welcome to My App</h1>
+    </div>
+  );
+}
+
+export default App;
